0f3e395dc725c0965ec45d3bcef39dc1c1ae2743
mojeda20 <141047560+moisesliotawork@users.noreply.github.com>
2024-07-19 23:46:14 -0400

    landing casi listo :+1:

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,49 @@
-import { FaInstagram, FaFacebook, FaLinkedin } from "react-icons/fa";
+import {
+  FaInstagram,
+  FaFacebook,
+  FaLinkedin,
+  FaWhatsapp,
+  FaTwitter,
+} from "react-icons/fa";
+import { BsTiktok } from "react-icons/bs";
+import venupLogo from "/venUp.png"; // Asegúrate de actualizar la ruta al logo de VenUp
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white text-center py-4">
-      <p>&copy; 2024 KFC. Todos los derechos reservados.</p>
-      <div className="flex justify-center space-x-4 mt-2">
-        <a href="#" className="text-white">
-          <FaInstagram />
-        </a>
-        <a href="#" className="text-white">
-          <FaFacebook />
-        </a>
-        <a href="#" className="text-white">
-          <FaLinkedin />
-        </a>
+    <footer className="footer">
+      <div className="footer-left">
+        <img src={venupLogo} alt="VenUp Logo" className="footer-logo" />
+        <div className="footer-left-text">
+          <p>¿Te gusto esta página?</p>
+          <p>Nosotros te hacemos la tuya</p>
+          <button className="bg-white text-red-600 px-4 py-2 rounded-full footer-left-button">
+            Venup
+          </button>
+        </div>
       </div>
-      <div className="mt-4">
-        <img src="venup-logo.png" alt="VenUp Logo" className="w-16 mx-auto" />
+      <div className="footer-separator"></div>
+      <div className="footer-right">
+        <p>Redes Sociales - KFC</p>
+        <div className="footer-right-icons">
+          <a href="#" className="text-white">
+            <FaInstagram />
+          </a>
+          <a href="#" className="text-white">
+            <FaWhatsapp />
+          </a>
+          <a href="#" className="text-white">
+            <BsTiktok />
+          </a>
+          <a href="#" className="text-white">
+            <FaTwitter />
+          </a>
+          <a href="#" className="text-white">
+            <FaFacebook />
+          </a>
+          <a href="#" className="text-white">
+            <FaLinkedin />
+          </a>
+        </div>
       </div>
     </footer>
   );
